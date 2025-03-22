35f9d345fe6a9180760a6670f2f59af23caf7856
HaileyFamo <haileyzhong.2022@gmail.com>
2025-03-21 21:56:12 -0400

    update README file

--- pixrefer/interface/run_rel.sh
+++ pixrefer/interface/run_rel.sh
@@ -2,4 +2,4 @@ python pixrefer/interface/interface_rel_mask.py \
     --json_path Pixrefer_data/data/rel_user_input/gpt4o_concise_results.json \
     --image_dir Pixrefer_data/data/images \
     --mask_dir Pixrefer_data/data/masks \
-    --output_dir output/user_rel
+    --output_dir output/user_rel/concise
